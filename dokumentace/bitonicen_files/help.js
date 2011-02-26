4f8beca77ab95b6057be4b59e008f453cbf7697f
Pavel <pavel.benacek@gmail.com>
2011-02-26 14:51:49 +0100

    pridana dokumentace k jednotlivym alg.

---
+++ dokumentace/bitonicen_files/help.js
@@ -0,0 +1,10 @@
+function help(x)
+{
+     window.open(x, 'test', 'width=700,height=250,menubar=no,location=no,scrollbars=no,dependent=yes')
+}
+
+
+function wind(x, w, h, s)
+{
+     window.open(x, 'test', 'width='+w+',height='+h+',menubar=no,location=no,scrollbars='+s+',dependent=yes')
+}
