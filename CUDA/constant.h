160847d4422d29e8cb0deddab9c541e883a255b2
Pavel Benacek <pavel.benacek@gmail.com>
2011-04-21 12:33:01 +0200

    mensi kosmeticke upravy v kodu

--- CUDA/constant.h
+++ CUDA/constant.h
@@ -1,10 +1,10 @@
 #ifndef CONSTANT_H_
 #define CONSTANT_H_
 
-#define ARRAY_SIZE 32
+#define ARRAY_SIZE 512
 #define MODULE 100
 
-#define NUM_OF_THREADS 16
+#define NUM_OF_THREADS 32
 
 /*konstanty pro faze*/
 #define SL 0
