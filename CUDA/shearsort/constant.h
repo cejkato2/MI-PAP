fd2b5c08655ecc8bf3ebee595068620517c51654
Pavel Benacek <pavel.benacek@gmail.com>
2011-04-28 13:30:43 +0200

    opravena verze

--- CUDA/shearsort/constant.h
+++ CUDA/shearsort/constant.h
@@ -1,10 +1,11 @@
 #ifndef CONSTANT_H_
 #define CONSTANT_H_
 
-#define ARRAY_SIZE 16384
+#define ARRAY_SIZE 32768
 #define MODULE 100
 
-#define NUM_OF_THREADS 256
+#define NUM_OF_THREADS 512
+
 
 /*konstanty pro faze*/
 #define SL 0
